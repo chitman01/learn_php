516777ce6e73b198aa0bc3c33f9e73ddfb08ecc5
chitman01 <ball4128ok@gmail.com>
2019-04-22 16:32:07 +0700

    edit commit

--- index.php
+++ index.php
@@ -40,40 +40,7 @@
 		});
 		});
 
-		$(document).ready(function(){
-			$(document).on('click','a[data-role=update]',function(){
-				//alert($(this).data('id'))
-				//alert($(this).data('firstName'))
-				var  id = $(this).data('id');
-				var  firstName = $('#'+id).children('td[data-target=firstName]').text();
-				var  lastName = $('#'+id).children('td[data-target=lastName]').text();
-				var  email = $('#'+id).children('td[data-target=email]').text();
-				var  t_number = $('#'+id).children('td[data-target=t_number]').text();
-
-				$('#firstName').val(firstName);
-				$('#lastName').val(lastName);
-				$('#email').val(email);
-				$('#t_number').val(t_number);
-				$('#userId').val(id);
-				$('#myModal').modal('toggle');
-			})
-		});
-		$('#save').click(function(){
-			var id = $('#userId').val();
-			var firstName = $('firstName').val();
-			var lastName = $('#lastName').val();
-			var email = $('#email').val();
-			var t_number = $('#t_number').val();
-
-			$.ajax({
-				url		:	'',
-				method	:	'post';
-				data 	:	{firstName : firstName , lastName : lastName , email : email , id : id},
-				success : function(response) {
-								console.log(response);
-							}
-			})
-		})
+		
 	</script>
 
 </head>
@@ -112,62 +79,61 @@
 				<td><div align="center" data-target="t_number"><?php echo $result["t_number"];?></div></td>
 				<td width="45" height="20" align="center">
 					<a href="#<?=$result['id']?>" class="delItem">delete</a>
-					<a href="#" data-role="update" data-id="<?= $result['id'] ?>">update</a>
+					<a href="#" data-role="update" data-id="<?php echo $result['id'] ;?>">Update</a>
 				</td>
 			</tr>
 			<!--delete	 https://www.ninenik.com/%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%9E%E0%B8%B4%E0%B9%88%E0%B8%A1_%E0%B8%A5%E0%B8%9A_%E0%B9%81%E0%B8%81%E0%B9%89%E0%B9%84%E0%B8%82_%E0%B9%81%E0%B8%9A%E0%B9%88%E0%B8%87%E0%B8%AB%E0%B8%99%E0%B9%89%E0%B8%B2_%E0%B8%82%E0%B9%89%E0%B8%AD%E0%B8%A1%E0%B8%B9%E0%B8%A5_%E0%B8%94%E0%B9%89%E0%B8%A7%E0%B8%A2_jquery_ajax_%E0%B8%AD%E0%B8%A2%E0%B9%88%E0%B8%B2%E0%B8%87%E0%B8%87%E0%B9%88%E0%B8%B2%E0%B8%A2-372.html 
 			post 	https://www.w3schools.com/jquery/jquery_ajax_get_post.asp
 			-->
-
-			<!-- Model -->
-			<div class="modal" id="myModal" role="dialog">
-				<div class="modal-dialog">
-				<div class="modal-content">
-				
-					<!-- Modal Header -->
-					<div class="modal-header">
-						<h4 class="modal-title">Update Database</h4>
-						<button type="button" class="close" data-dismiss="modal">&times;</button>
-					</div>
-					<!-- Modal body -->
-					<div class="modal-body">
-						<div class="form-group">
-							<label>ID</label>
-							<input type="hidden" id="id" class="form-control">
-						</div>
-						<div class="form-group">
-							<label>FistName</label>
-							<input type="text" id="fistName" class="form-control">
-						</div>
-						<div class="form-group">
-							<label>LastName</label>
-							<input type="text" id="lastName" class="form-control">
-						</div>
-						<div class="form-group">
-							<label>Email</label>
-							<input type="text" id="email" class="form-control">
-						</div>
-						<div class="form-group">
-							<label>Phone</label>
-							<input type="text" id="phone" class="form-control">
-						</div>
-					</div>
-					
-					<!-- Modal footer -->
-					<div class="modal-footer">
-						<a href="#" id="save" class="btn btn-primary pull-left">Update</a>
-						<button type="button" class="btn btn-danger pull-right" data-dismiss="modal">Close</button>
-					</div>
-					
-				</div>
-				</div>
-			</div>
-
 		<?php
 		}
 		?>
 		</table>
 	</form>
+	
+	<!-- Model -->
+	<div id="myModal" class="modal fade" role="dialog">
+		<div class="modal-dialog">
+
+		<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Update Modal</h4>
+			</div>
+			<!-- Modal body -->
+			<div class="modal-body">
+				<div class="form-group">
+					<label>FistName</label>
+					<input type="text" id="firstName" class="form-control">
+				</div>
+				<div class="form-group">
+					<label>LastName</label>
+					<input type="text" id="lastName" class="form-control">
+				</div>
+				<div class="form-group">
+					<label>Email</label>
+					<input type="text" id="email" class="form-control">
+				</div>
+				<div class="form-group">
+					<label>Phone</label>
+					<input type="text" id="t_number" class="form-control">
+				</div>
+				<input type="hidden" id="userId" class="form-control">
+			</div>
+					
+			<!-- Modal footer -->
+			<div class="modal-footer">
+				<a href="#" id="save" class="btn btn-primary pull-right">Update</a>
+				<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+			</div>	
+		</div>
+		</div>
+	</div>
+
+
+	
+
 	<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Update Database</button>
 		<!-- https://www.youtube.com/watch?v=aujNp92p0Uc 
 			http://developer-zone.000webhostapp.com/2017/09/update-data-using-jquery-ajax-php-and-mysql
@@ -201,4 +167,44 @@
 </div>
 
 </body>
+
+<script>
+		$(document).ready(function(){
+
+			$(document).on('click','a[data-role=update]',function(){
+				alert($(this).data('id'))
+				//alert($(this).data('firstName'))
+				var id  = $(this).data('id');
+				var firstName = $('#'+id).children('td[data-target=firstName]').text();
+				var lastName = $('#'+id).children('td[data-target=lastName]').text();
+				var email = $('#'+id).children('td[data-target=email]').text();
+				var t_number = $('#'+id).children('td[data-target=t_number]').text();
+
+				$('#firstName').val(firstName);
+				$('#lastName').val(lastName);
+				$('#email').val(email);
+				$('#t_number').val(t_number);
+				$('#userId').val(id);
+				$('#myModal').modal('toggle');
+			});
+		
+			$('#save').click(function(){
+				var id = $('#userId').val();
+				var firstName = $('firstName').val();
+				var lastName = $('#lastName').val();
+				var email = $('#email').val();
+				var t_number = $('#t_number').val();
+
+				$.ajax({
+					url		:	'',
+					method	:	'post';
+					data 	:	{firstName : firstName , lastName : lastName , email : email , id : id},
+					success : function(response) {
+									console.log(response);
+								}
+				})
+			});
+		});
+	</script>
+
 </html>
--- post.php
+++ post.php
@@ -1,19 +1,118 @@
 <?php
+  include_once "./database/condition.php";
 
-    $str_a = '1234567890';
-    $count_a = strlen($str_a);
-    echo $str_a."  ".$count_a."<br>";
+  $sql = "SELECT * FROM test_1";
     
-    $str_split_a = str_split($str_a);
-
-    print_r($str_split_a);
-
-    echo implode("",$str_split_a)."<br>";
-
-
-
-    $sub_str_a = substr($str_a,0,9);
-
-    echo $sub_str_a;
-
+	$stmt = $conn->prepare($sql);
+	$stmt->execute();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Ajax Update</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+<div align="center">
+	<form action="" name="Main" id="Main" method="post">
+		<table class="table">
+			<tr>
+				<th width="50"> <div align="center">ID</div></th>
+				<th width="100"> <div align="center">FistName </div></th>
+				<th width="100"> <div align="center">LastName </div></th>
+				<th width="250"> <div align="center">Email </div></th>
+				<th width="100"> <div align="center">Phone </div></th>
+				<th width="200"> <div align="center">Option </div></th>
+			</tr>
+		<?php
+		while($result = $stmt->fetch( PDO::FETCH_ASSOC )){	
+		?>
+			<tr>
+				<td><div name="txtid" id="txtid" align="center"><?php echo $result["id"];?></div></td>
+				<td><div align="center" data-target="firstName"><?php echo $result["fname"];?></div></td>
+				<td><div align="center" data-target="lastName"><?php echo $result["lname"];?></div></td>
+				<td><div align="center" data-target="email"><?php echo $result["email"];?></div></td>
+				<td><div align="center" data-target="t_number"><?php echo $result["t_number"];?></div></td>
+				<td width="45" height="20" align="center">
+					<a href="#<?=$result['id']?>" data-role="update" data-id="<?php echo $result['id'] ;?>">Update</a>
+				</td>
+			</tr>
+		<?php
+		}
+		?>
+		</table>
+	</form>
+	
+	<!-- Model -->
+	<div id="myModal" class="modal fade" role="dialog">
+		<div class="modal-dialog">
+
+		<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Update Modal</h4>
+			</div>
+			<!-- Modal body -->
+			<div class="modal-body">
+				<div class="form-group">
+					<label>FistName</label>
+					<input type="text" id="firstName" class="form-control">
+				</div>
+				<div class="form-group">
+					<label>LastName</label>
+					<input type="text" id="lastName" class="form-control">
+				</div>
+				<div class="form-group">
+					<label>Email</label>
+					<input type="text" id="email" class="form-control">
+				</div>
+				<div class="form-group">
+					<label>Phone</label>
+					<input type="text" id="t_number" class="form-control">
+				</div>
+				<input type="hidden" id="userId" class="form-control">
+			</div>
+					
+			<!-- Modal footer -->
+			<div class="modal-footer">
+				<a href="#" id="save" class="btn btn-primary pull-right">Update</a>
+				<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+			</div>	
+		</div>
+		</div>
+	</div>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Update Database</button>
+
+</body>
+
+<script>
+		$(document).ready(function(){
+
+			$(document).on('click','a[data-role=update]',function(){
+				//alert($(this).data('id'));
+				//alert($(this).data('firstName'))
+				var id  = $(this).data('id');
+				var firstName = $('#'+id).children('td[data-target=firstName]').text();
+				var lastName = $('#'+id).children('td[data-target=lastName]').text();
+				var email = $('#'+id).children('td[data-target=email]').text();
+				var t_number = $('#'+id).children('td[data-target=t_number]').text();
+
+				$('#firstName').val(firstName);
+				$('#lastName').val(lastName);
+				$('#email').val(email);
+				$('#t_number').val(t_number);
+				$('#userId').val(id);
+                $('#myModal').modal('toggle');
+			});
+		
+		});
+	</script>
+
+</html>
